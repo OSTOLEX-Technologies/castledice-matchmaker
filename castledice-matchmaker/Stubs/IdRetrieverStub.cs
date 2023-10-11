c255ea5df54b899c01dabe163bb25a1550f77c11
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-11 22:28:23 +0300

    Wrote entry point and added stubs. Also added config files and logger.

---
+++ castledice-matchmaker/Stubs/IdRetrieverStub.cs
@@ -0,0 +1,14 @@
+﻿using casltedice_events_logic.ClientToServer;
+
+namespace castledice_matchmaker.Stubs;
+
+/// <summary>
+/// This class MUST NOT be used in a production code.
+/// </summary>
+public class IdRetrieverStub : IIdRetriever
+{
+    public int RetrievePlayerId(RequestGameDTO dto)
+    {
+        return 1;
+    }
+}
