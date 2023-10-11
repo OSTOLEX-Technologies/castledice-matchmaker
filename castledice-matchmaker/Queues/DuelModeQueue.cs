b17eb9f0579e5281e4b16b18bb90c0bd709d5cf3
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-11 18:11:33 +0300

    Wrote DuelModeQueue.

--- castledice-matchmaker/Queues/DuelModeQueue.cs
+++ castledice-matchmaker/Queues/DuelModeQueue.cs
@@ -4,15 +4,25 @@ namespace castledice_matchmaker.Queues;
 
 public class DuelModeQueue : IGameModeQueue
 {
+    private readonly Queue<int> _waitingPlayersIds = new();
+    
     public void EnqueuePlayer(int playerId)
     {
-        throw new NotImplementedException();
+        _waitingPlayersIds.Enqueue(playerId);
     }
 
     public List<Match> GetMatches()
     {
-        throw new NotImplementedException();
+        var matches = new List<Match>();
+        while (_waitingPlayersIds.Count >= 2)
+        {
+            var firstPlayerId = _waitingPlayersIds.Dequeue();
+            var secondPlayerId = _waitingPlayersIds.Dequeue();
+            var match = new DuelMatch(firstPlayerId, secondPlayerId);
+            matches.Add(match);
+        }
+        return matches;
     }
 
-    public GameMode GameMode { get; }
+    public GameMode GameMode => GameMode.Duel;
 }
